05acb4975a20397ac77e5dc9a2cd6c42a0c41188
Muralidhar22 <muralidhar7794@gmail.com>
2023-03-05 21:07:23 +0530

    config and setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter } from '@next/font/google'
+import { SessionProvider } from "next-auth/react"
+
+const inter = Inter({ subsets: ['latin'], weight: ["400","500","600","700"],variable: "--font-inter"  })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${inter.variable} font-sans`}>
+    <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+    </SessionProvider>
+  </div>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,9 @@ module.exports = {
   theme: {
     extend: {},
   },
+  fontFamily: {
+    sans: ["var(--font-inter)"],
+  },
+  darkMode: 'class',
   plugins: [],
 }
