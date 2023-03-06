b1997b1da2d162a6b5fb0d6c51ff219463f3a754
Muralidhar22 <muralidhar7794@gmail.com>
2023-03-06 18:20:20 +0530

    setup refactor

---
+++ src/pages/bookmarks.tsx
@@ -0,0 +1,7 @@
+const BookMarks = () => {
+    return(<>
+        <h1>Book marks</h1>
+    </>)
+}
+
+export default BookMarks;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      black: "rgb(var(--black))",
+      navy: "rgb(var(--navy))",
+      yellow: "rgb(var(--yellow))"
+    }
   },
   fontFamily: {
     sans: ["var(--font-inter)"],
   },
-  darkMode: 'class',
   plugins: [],
 }
